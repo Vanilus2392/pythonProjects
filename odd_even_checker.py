15d20b997449c1211de34303f2ae3840458863bf
Joash Casiño <vanillagreen43@gmail.com>
2025-08-23 09:24:38 +0800

    n

--- odd_even_checker.py
+++ odd_even_checker.py
@@ -1,22 +1,24 @@
-def is_even(n):
-    """Check if a number is even."""
-    if n % 2 == 0:
-        return 'even'
-    else:
-        return 'odd'
+from random import randrange
 
+
+def game():
+    pass
+    
 def main():
-    while True:
-        try:
-            user_input = input("Enter a number: ")
-            number = int(user_input)
-            result = is_even(number)
-            print(f"The number {number} is {result}.")
-        except ValueError:
-            print("Please enter a valid integer.")
-        user_input = input("Do you want to check another number? (yes/no): ")
-        if user_input.lower() != 'yes':
-            print("Exiting the program.")
-            break
+    player = int(input("Rock[0], Paper[1], Scissors[2]? "))
+    computer = randrange(2)
 
-main()
+    print(f"Computer: {computer}")
+
+    if player < 0 or player > 2:
+        print("Invalid option")
+    elif (player == 0 and computer == 1) or (player == 1 and computer == 2) or (player == 2 and computer == 0):
+        print("Computer wins!")
+    elif (player == 1 and computer == 0) or (player == 2 and computer == 1) or (player == 0 and computer == 2):
+        print("Player wins!")
+    else:
+        print("Draw!")
+
+if __name__ == "__main__":
+    main()
+    
